cc3b26bdc9b05879b123fd27976539dea621cd46
thando mini <tzone85@gmail.com>
2019-02-09 18:23:57 +0200

    made use of the Object.create way of creating objects and their instances

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,6 +1,6 @@
 // Function constructor
 
-var Person  = function(name, yearOfBirth, job){
+/*var Person  = function(name, yearOfBirth, job){
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
@@ -25,3 +25,24 @@ jane.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+
+// Object.create
+
+var personProto = {
+    calculateAge : function() {
+        console.log(2019 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+
+var jane = Object.create(personProto, {
+    name: { value: 'Jane'},
+    yearOfBirth: { value: 1969},
+    job: { value: 'designer'}
+});
